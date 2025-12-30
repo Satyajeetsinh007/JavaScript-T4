de166415a01d53533956c05038e8d1f472ea2a38
Satyajeetsinh007 <satyajadeja52@gmail.com>
2025-12-30 10:48:53 +0530

    Add files via upload

---
+++ Lec4/DOM1.js
@@ -0,0 +1,21 @@
+div1=document.getElementById("id1");
+p=document.querySelectorAll("p");
+
+function fun1(){
+    //div1.setAttribute("backgroundColor","red");
+//     div1.style.color="red";
+div1.style.backgroundColor="yellow";
+//    div1.setAttribute("backgroundColor","red")
+//     div1.style.border="2px solid blue";
+//     div1.style.padding="10px";
+//     div1.style.width="200px";
+//     div1.style.textAlign="center";
+//     div1.style.boxShadow="5px 5px 10px gray";
+//     div1.style.boxSizing="border-box";
+//     for(i in p){
+//         p[i].style.color="red";
+
+//     }
+   
+//div1.setAttribute("style","background-color:red")
+}
